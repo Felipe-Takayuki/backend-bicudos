675c4ba2f78fb2428b6b1becbd8928cf40f6404a
Felipe-Takayuki <ftanaka91@gmail.com>
2026-09-24 13:05:17 -0300

    chore: setup inicial do projeto e infraestrutura docker com postgis

---
+++ docker/init-postgis.sql
@@ -0,0 +1,4 @@
+-- Habilita a extensão PostGIS para operações e funções geoespaciais avançadas (SIG / QGIS)
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
